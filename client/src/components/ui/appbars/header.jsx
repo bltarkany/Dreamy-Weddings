f4e23c32a339927cfd611bf38b1d1508a814408c
Bobbi Tarkany <bl.tarkany@gmail.com>
2024-02-27 12:22:34 -0800

    updated header icons and links

--- client/src/components/ui/appbars/header.jsx
+++ client/src/components/ui/appbars/header.jsx
@@ -12,7 +12,7 @@ export const Header = () => {
             <img
               src="./assets/imgs/logononame.png"
               className="h-8 rounded"
-              alt="Flowbite Logo"
+              alt="bobbi tarkany logo"
             />
             <span className="self-center text-2xl font-header font-semibold whitespace-nowrap dark:text-white">
               Dreamy Wedding
@@ -46,7 +46,7 @@ export const Header = () => {
             <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
               <li>
                 <Link
-                  href="#"
+                  to="/"
                   className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
                   aria-current="page"
                 >
@@ -55,7 +55,7 @@ export const Header = () => {
               </li>
               <li>
                 <Link
-                  href="#"
+                  to="/engagement"
                   className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                 >
                   Engagement
@@ -63,7 +63,7 @@ export const Header = () => {
               </li>
               <li>
                 <Link
-                  href="#"
+                  to="/events"
                   className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                 >
                   Events
@@ -71,7 +71,7 @@ export const Header = () => {
               </li>
               <li>
                 <Link
-                  href="#"
+                  to="/gallery"
                   className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                 >
                   Gallery
@@ -79,7 +79,7 @@ export const Header = () => {
               </li>
               <li>
                 <Link
-                  href="#"
+                  to="/contact"
                   className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                 >
                   Contact
