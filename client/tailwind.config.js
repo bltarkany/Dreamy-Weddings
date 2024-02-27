5c84981ffaa2d6ea5c72cc03260ad54a1c8842f0
Bobbi Tarkany <bl.tarkany@gmail.com>
2024-02-27 12:36:31 -0800

    dark mode selector to config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -7,6 +7,7 @@ export default {
     './node_modules/flowbite/**/*.js',
     'node_modules/flowbite-react/lib/esm/**/*.js',
   ],
+  darkMode: 'selector',
   theme: {
     extend: {
       transitionDuration: {
