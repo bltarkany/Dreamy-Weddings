b2d9df5dec9df3350a976d8287b92cb5b2f28201
Bobbi Tarkany <bl.tarkany@gmail.com>
2024-02-29 17:44:46 -0800

    updated export

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,4 +1,5 @@
 const userControllers = require('./userControllers');
 const eventControllers = require('./eventControllers');
+const evt_gstControllers = require('./evt_guestControllers');
 
-module.exports = { userControllers, eventControllers };
+module.exports = { userControllers, eventControllers, evt_gstControllers };
