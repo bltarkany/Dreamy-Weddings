49b696978e40841217453e598589540ba877fa01
Bobbi Tarkany <bl.tarkany@gmail.com>
2024-03-05 15:28:34 -0800

    created create and update event controllers

--- server/controllers/eventControllers.js
+++ server/controllers/eventControllers.js
@@ -1,6 +1,31 @@
-// TODO: create event
-// TODO: update event
-// TODO: delete event
-// TODO: get all events
-// TODO: get single event
-// TODO: get event with guest count
+const { Event } = require('../models');
+
+module.exports = {
+  // TODO: create event
+  async createEvent({ user, body }, res) {
+    try {
+      const event = await Event.create(body);
+      res.status(201).json(event);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  },
+  // TODO: update event
+  async updateEvent({ user, body, params }, res) {
+    try {
+      const event = await Event.update(body, {
+        where: { id: params.id },
+      });
+      if (!event) {
+        return res.status(404).json({ message: 'No event found.' });
+      }
+      res.status(202).json(event);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  },
+  // TODO: delete event
+  // TODO: get all events
+  // TODO: get single event
+  // TODO: get event with guest count
+};
