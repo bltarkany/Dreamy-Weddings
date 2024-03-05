bcd31a24c3570c2bd5bb362f86dbc4dc648d0cca
Bobbi Tarkany <bl.tarkany@gmail.com>
2024-03-05 14:00:45 -0800

    added create user controller needs auth

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -1,5 +1,29 @@
-// TODO: create user
 // TODO: login user
 // TODO: update user
 // TODO: get users
 // TODO: get user by id
+const { User } = require('../models');
+
+module.exports = {
+  // create user
+  async signUp({ body }, res) {
+    try {
+      const user = await User.create(body);
+      // TODO - add auth here
+
+      res.status(201).json(user);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  },
+  // login user
+  async login({ body }, res) {
+    try {
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  },
+  // update user
+  // get users
+  // get user by id
+};
