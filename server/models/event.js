044802794e5fad9a4cb248e56ca3672ae3fd9eb0
Bobbi Tarkany <bl.tarkany@gmail.com>
2024-03-05 13:34:47 -0800

    created event model

--- server/models/event.js
+++ server/models/event.js
@@ -1,3 +1,57 @@
-// TODO: create event table
-// TODO: add columns - id, name, date, time, address1, addres2, city, state, zip, image
-// TODO: add exportations
+const { DataTypes, Model } = require('sequelize');
+
+class Event extends Model {}
+
+Event.init(
+  {
+    id: {
+      type: DataTypes.SMALLINT,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    evt_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    host_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    date: {
+      type: DataTypes.DATEONLY,
+      allowNull: false,
+    },
+    time: {
+      type: DataTypes.TIME,
+      allowNull: false,
+    },
+    address_one: {
+      type: DataTypes.STRING,
+    },
+    address_two: {
+      type: DataTypes.STRING,
+    },
+    city: {
+      type: DataTypes.STRING,
+    },
+    state: {
+      type: DataTypes.STRING,
+    },
+    zip: {
+      type: DataTypes.INTEGER,
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'event',
+  }
+);
+
+module.exports = Event;
