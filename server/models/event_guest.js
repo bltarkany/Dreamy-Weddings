4abd9aa1462e03d2168949a8b9d79b5755ac17f8
Bobbi Tarkany <bl.tarkany@gmail.com>
2024-03-05 13:40:01 -0800

    created event guest join model

---
+++ server/models/event_guest.js
@@ -0,0 +1,36 @@
+const { DataTypes, Model } = require('sequelize');
+
+class EventGuest extends Model {}
+
+EventGuest.init(
+  {
+    id: {
+      type: DataTypes.SMALLINT,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    event_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'event',
+        key: 'id',
+      },
+    },
+    guest_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'guest',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'eventguest',
+  }
+);
+
+module.exports = EventGuest;
