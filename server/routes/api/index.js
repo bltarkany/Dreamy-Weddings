1f73b25c270a3a0f2c1304c6045827ce658bc3f1
Bobbi Tarkany <bl.tarkany@gmail.com>
2024-03-05 11:52:20 -0800

    created index.js restful api routing mid points

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,4 +1,14 @@
-// TODO: route to each with restful api standards
-// TODO: require all routes files
-// TODO: require router
-// TODO: export router
+const router = require('express').Router();
+const evtRoutes = require('./eventRoutes');
+const userRoutes = require('./userRoutes');
+const guestRoutes = require('./guestRoutes');
+const wedpartyRoutes = require('./wedpartyRoutes');
+const galleryRoutes = require('./galleryRoutes');
+
+router.use('/users', userRoutes);
+router.use('/events', evtRoutes);
+router.use('/guests', guestRoutes);
+router.use('/weddingparty', wedpartyRoutes);
+router.use('/galleries', galleryRoutes);
+
+module.exports = router;
