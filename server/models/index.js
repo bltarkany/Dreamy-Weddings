303aa1660ae9e82d6a03e93b87acb0551693f8bd
Bobbi Tarkany <bl.tarkany@gmail.com>
2024-03-05 13:50:07 -0800

    created model associations

--- server/models/index.js
+++ server/models/index.js
@@ -1,2 +1,28 @@
-// TODO: add in associations
-// TODO: create exportations
+const User = require('./user');
+const Event = require('./event');
+const Guest = require('./guest');
+const EventGuest = require('./event_guest');
+const WedParty = require('./weddingparty');
+const Gallery = require('./gallery');
+
+// associations
+Gallery.belongsTo(Event, {
+  foreignKey: 'event_id',
+  onDelete: 'SET NULL',
+});
+
+Event.hasMany(Gallery, {
+  foreignKey: 'event_id',
+});
+
+Event.belongsToMany(Guest, {
+  foreignKey: 'event_id',
+  through: EventGuest,
+});
+
+Guest.belongsToMany(Event, {
+  foreignKey: 'guest_id',
+  through: EventGuest,
+});
+
+module.exports = { User, Event, Guest, EventGuest, WedParty, Gallery };
