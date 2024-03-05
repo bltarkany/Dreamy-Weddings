91045f05fd5683b1870142e2830eebf2a865c8a0
Bobbi Tarkany <bl.tarkany@gmail.com>
2024-03-05 11:56:41 -0800

    created index.js api and production frontend urls

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,4 +1,13 @@
-// TODO: create html production output
-// TODO: require api folder
-// TODO: require router
-// TODO: export router
+const router = require('express').Router();
+const path = require('path');
+const apiRoutes = require('./api');
+
+router.use('/api', apiRoutes);
+
+// serve up react front-end in production
+// TEST - is this accurate for vite should it be dist not build??
+router.use((req, res) => {
+  res.sendFile(path.join(__dirname, '../../client/build/index.html'));
+});
+
+module.exports = router;
