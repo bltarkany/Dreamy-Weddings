1cfe0b494c64fe5a0f2517990aba86bb693b48a5
Bobbi Tarkany <bl.tarkany@gmail.com>
2024-03-05 12:14:55 -0800

    created wedding party model

--- server/models/weddingparty.js
+++ server/models/weddingparty.js
@@ -1,3 +1,64 @@
-// TODO: create wedding party table
-// TODO: add columns - id, image, first, last, email, role, intro
-// TODO: add exportations
+const { DataTypes, Model } = require('sequelize');
+
+class WedParty extends Model {}
+
+WedParty.init(
+  {
+    id: {
+      type: DataTypes.SMALLINT,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    first_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    last_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    address_one: {
+      type: DataTypes.STRING,
+    },
+    address_two: {
+      type: DataTypes.STRING,
+    },
+    city: {
+      type: DataTypes.STRING,
+    },
+    state: {
+      type: DataTypes.STRING,
+    },
+    zip: {
+      type: DataTypes.INTEGER,
+    },
+    role: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    custom_role: {
+      type: DataTypes.STRING,
+    },
+    intro: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'wedparty',
+  }
+);
+
+module.exports = WedParty;
