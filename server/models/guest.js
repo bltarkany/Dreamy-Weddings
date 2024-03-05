8d6e9371a78e2067a5e83f53b6c6771f6f68c9c3
Bobbi Tarkany <bl.tarkany@gmail.com>
2024-03-05 12:07:09 -0800

    created guest model

--- server/models/guest.js
+++ server/models/guest.js
@@ -1,3 +1,53 @@
-// TODO: create guest table
-// TODO: add columns - id, first, last, email, numberofguests
-// TODO: add exportations
+const { DataTypes, Model } = require('sequelize');
+
+class Guest extends Model {}
+
+Guest.init(
+  {
+    id: {
+      type: DataTypes.SMALLINT,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    first_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    last_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    address_one: {
+      type: DataTypes.STRING,
+    },
+    address_two: {
+      type: DataTypes.STRING,
+    },
+    city: {
+      type: DataTypes.STRING,
+    },
+    state: {
+      type: DataTypes.STRING,
+    },
+    zip: {
+      type: DataTypes.INTEGER,
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user',
+  }
+);
+
+module.exports = Guest;
