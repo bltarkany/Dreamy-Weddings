d6b0f87de0110ebaa118b64f95867beb966ba4f3
Bobbi Tarkany <bl.tarkany@gmail.com>
2024-03-05 13:34:06 -0800

    created gallery model

--- server/models/gallery.js
+++ server/models/gallery.js
@@ -1,3 +1,36 @@
-// TODO: create gallery table
-// TODO: add columns - id, image, event_id
-// TODO: add exportations
+const { DataTypes, Model } = require('sequelize');
+
+class Gallery extends Model {}
+
+Gallery.init(
+  {
+    id: {
+      type: DataTypes.SMALLINT,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    image: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    photographer: {
+      type: DataTypes.STRING,
+    },
+    event_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'event',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'gallery',
+  }
+);
+
+module.exports = Gallery;
