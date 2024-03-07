efc5bc5fa0c17366ad7df922d642ad9ad82a11a2
Bobbi Tarkany <bl.tarkany@gmail.com>
2024-03-06 19:30:03 -0800

    created todo issue list

---
+++ server/controllers/galleryController.js
@@ -0,0 +1,6 @@
+// TODO: create gallery item
+// TODO: delete gallery item
+// TODO: update gallery item
+// TODO: get all gallery items
+// TODO: get galley items by event
+// TODO: get single gallery item
