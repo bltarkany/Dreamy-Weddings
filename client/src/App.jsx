edb1c4ff8665def2a25f0c8fd509562f2fd18e3e
Bobbi Tarkany <bl.tarkany@gmail.com>
2024-03-19 12:19:24 -0700

    added outlet to App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,15 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import { Outlet } from 'react-router-dom';
 import Header from './components/ui/appbars/header';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <Header />
+      <main className="content">
+        <Outlet />
+      </main>
     </>
   );
 }
