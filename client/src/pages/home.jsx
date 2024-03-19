1d96fa74db3ccb017824d6e7218555399e114bf8
Bobbi Tarkany <bl.tarkany@gmail.com>
2024-03-19 15:08:39 -0700

    added carousel with use effect and default dependencies

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,8 +1,24 @@
+import { useEffect, useState } from 'react';
+import SlideCarousel from '../components/ui/banners/carousel';
+
+// default images
+const defaultImgs = [
+  'https://res.cloudinary.com/dvsxq5pie/image/upload/v1711477773/dreamy_wedding/home/home_couple_bfxqnd.jpg',
+  'https://res.cloudinary.com/dvsxq5pie/image/upload/v1711490428/dreamy_wedding/home/home_ring_wvwfle.jpg',
+  'https://res.cloudinary.com/dvsxq5pie/image/upload/v1711490278/dreamy_wedding/home/home_coupleback_szz0qa.jpg',
+];
+
 export default function Home() {
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    setImages(defaultImgs);
+  }, []);
+
   return (
     <>
+      <SlideCarousel images={images} />
       <h1>home page</h1>
-      makign changes
     </>
   );
 }
