c3c591497bd5b2e43ef6d7247cda20f23ac1df2c
Bobbi Tarkany <bl.tarkany@gmail.com>
2024-03-22 10:56:51 -0700

    created guest routes

--- server/routes/api/guestRoutes.js
+++ server/routes/api/guestRoutes.js
@@ -1,4 +1,25 @@
-// TODO: create for each controller
-// TODO: require model
-// TODO: require router
-// TODO: export router
+const router = require('express').Router();
+const { authMiddleware } = require('../../utils/auth');
+const {
+  guestControllers: {
+    createGuest,
+    updateGuest,
+    deleteGuest,
+    getGuests,
+    getGuestbyId,
+  },
+} = require('../../controllers');
+
+// /api/guests
+router
+  .route('/')
+  .get(authMiddleware, getGuests)
+  .post(authMiddleware, createGuest);
+// /api/guests/:id
+router
+  .route('/:id')
+  .get(authMiddleware, getGuestbyId)
+  .put(authMiddleware, updateGuest)
+  .delete(authMiddleware, deleteGuest);
+
+module.exports = router;
