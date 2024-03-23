aa12b4e19e11eb6e94fddc398785c18dcd9ba3e6
Bobbi Tarkany <bl.tarkany@gmail.com>
2024-03-23 10:07:36 -0700

    updated build delivery

--- server/routes/index.js
+++ server/routes/index.js
@@ -7,7 +7,7 @@ router.use('/api', apiRoutes);
 // serve up react front-end in production
 // TEST - is this accurate for vite should it be dist not build??
 router.use((req, res) => {
-  res.sendFile(path.join(__dirname, '../../client/build/index.html'));
+  res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
 });
 
 module.exports = router;
