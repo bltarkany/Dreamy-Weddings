9b8d7ce4ce1634e35e0c97b05e308064c379c450
Bobbi Tarkany <bl.tarkany@gmail.com>
2024-03-23 10:37:51 -0700

    created wedding party routes

--- server/routes/api/wedpartyRoutes.js
+++ server/routes/api/wedpartyRoutes.js
@@ -1,4 +1,23 @@
-// TODO: create for each controller
-// TODO: require model
-// TODO: require router
-// TODO: export router
+const router = require('express').Router();
+const { authMiddleware } = require('../../utils/auth');
+const {
+  wedPartyControllers: {
+    createMember,
+    updateMember,
+    deleteMember,
+    getMembers,
+    getMemberById,
+  },
+} = require('../../controllers');
+const { get } = require('./evt_guestRoutes');
+
+// /api/weddingparty/
+router.route('/').get(getMembers).post(authMiddleware, createMember);
+// /api/weddingparty/:id
+router
+  .route('/:id')
+  .get(getMemberById)
+  .put(authMiddleware, updateMember)
+  .delete(authMiddleware, deleteMember);
+
+module.exports = router;
