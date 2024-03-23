ecf5074d8d0ae2cadaa32b58c19e84ec7e008ad4
Bobbi Tarkany <bl.tarkany@gmail.com>
2024-03-23 10:33:34 -0700

    created user routes

--- server/routes/api/userRoutes.js
+++ server/routes/api/userRoutes.js
@@ -1,4 +1,17 @@
-// TODO: create for each controller
-// TODO: require model
-// TODO: require router
-// TODO: export router
+const router = require('express').Router();
+const { authMiddleware } = require('../../utils/auth');
+const {
+  userControllers: { signUp, login, updateUser, getUsers, getUserById },
+} = require('../../controllers');
+
+// /api/users/
+router.route('/').get(getUsers).post(signUp);
+// /api/users/login
+router.route('/login').post(login);
+// /api/users/:id
+router
+  .route('/:id')
+  .get(authMiddleware, getUserById)
+  .put(authMiddleware, updateUser);
+
+module.exports = router;
