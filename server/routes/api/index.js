5d3513f3492bd4610410cb53238abf6bd9874c9c
Bobbi Tarkany <bl.tarkany@gmail.com>
2024-03-23 10:12:50 -0700

    updated exports for new routes

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -4,11 +4,15 @@ const userRoutes = require('./userRoutes');
 const guestRoutes = require('./guestRoutes');
 const wedpartyRoutes = require('./wedpartyRoutes');
 const galleryRoutes = require('./galleryRoutes');
+const layoutRoutes = require('./layoutRoutes');
+const evt_guestRoutes = require('./evt_guestRoutes');
 
 router.use('/users', userRoutes);
 router.use('/events', evtRoutes);
 router.use('/guests', guestRoutes);
 router.use('/weddingparty', wedpartyRoutes);
 router.use('/galleries', galleryRoutes);
+router.use('/layouts', layoutRoutes);
+router.use('/evtguests', evt_guestRoutes);
 
 module.exports = router;
