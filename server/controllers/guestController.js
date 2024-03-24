be8d6e8b18b882921e9db451d45ff33c62766c54
Bobbi Tarkany <bl.tarkany@gmail.com>
2024-03-24 09:47:05 -0700

    create guest - guest controllers

--- server/controllers/guestController.js
+++ server/controllers/guestController.js
@@ -1,6 +1,53 @@
-// TODO: create guest
-// TODO: update guest
-// TODO: delete guest
-// TODO: get all guests
-// TODO: get one guest
-// TODO: get guest count
+const { Event, Guest, EventGuest } = require('../models');
+
+module.exports= {
+    // TODO: create guest
+    async createGuest({user, body}, res){
+        try {
+            const guest = await Guest.create(body);
+            res.status(201).json(guest);
+        } catch (error) {
+            res.status(500).json(error);
+        }
+    }
+    // TODO: update guest
+        async updateGuest({user, body, params}, res){
+        try {
+            
+        } catch (error) {
+            res.status(500).json(error);
+        }
+    }
+    // TODO: delete guest
+        async deleteGuest({user, params}, res){
+        try {
+            
+        } catch (error) {
+            res.status(500).json(error);
+        }
+    }
+    // TODO: get all guests
+        async getGuests({user}, res){
+        try {
+            
+        } catch (error) {
+            res.status(500).json(error);
+        }
+    }
+    // TODO: get one guest
+        async getGuestbyId({user, params}, res){
+        try {
+            
+        } catch (error) {
+            res.status(500).json(error);
+        }
+    }
+    // TODO: get guest count
+    // async createGuest({user, body}, res){
+    //     try {
+            
+    //     } catch (error) {
+    //         res.status(500).json(error);
+    //     }
+    // }
+}
