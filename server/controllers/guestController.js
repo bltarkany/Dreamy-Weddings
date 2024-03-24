2cc9c1b0ec41ebc16b9801aba132e54f5cf8f398
Bobbi Tarkany <bl.tarkany@gmail.com>
2024-03-24 09:48:57 -0700

    update guest - guest controllers

--- server/controllers/guestController.js
+++ server/controllers/guestController.js
@@ -1,53 +1,60 @@
 const { Event, Guest, EventGuest } = require('../models');
 
-module.exports= {
-    // TODO: create guest
-    async createGuest({user, body}, res){
-        try {
-            const guest = await Guest.create(body);
-            res.status(201).json(guest);
-        } catch (error) {
-            res.status(500).json(error);
-        }
+module.exports = {
+  // TODO: create guest
+  async createGuest({ user, body }, res) {
+    try {
+      const guest = await Guest.create(body);
+      res.status(201).json(guest);
+    } catch (error) {
+      res.status(500).json(error);
     }
-    // TODO: update guest
-        async updateGuest({user, body, params}, res){
-        try {
-            
-        } catch (error) {
-            res.status(500).json(error);
-        }
+  },
+  // TODO: update guest
+  async updateGuest({ user, body, params }, res) {
+    try {
+      const guest = await Guest.update(body, {
+        where: {
+          id: params.id,
+        },
+      });
+      if (!guest) {
+        return res
+          .status(404)
+          .json({ message: 'No guest with the selected id.' });
+      }
+      res.status(201).json(guest);
+    } catch (error) {
+      res.status(500).json(error);
     }
-    // TODO: delete guest
-        async deleteGuest({user, params}, res){
-        try {
-            
-        } catch (error) {
-            res.status(500).json(error);
-        }
+  },
+  // TODO: delete guest
+  async deleteGuest({ user, params }, res) {
+    try {
+    } catch (error) {
+      res.status(500).json(error);
     }
-    // TODO: get all guests
-        async getGuests({user}, res){
-        try {
-            
-        } catch (error) {
-            res.status(500).json(error);
-        }
+  },
+  // TODO: get all guests
+  async getGuests({ user }, res) {
+    try {
+    } catch (error) {
+      res.status(500).json(error);
     }
-    // TODO: get one guest
-        async getGuestbyId({user, params}, res){
-        try {
-            
-        } catch (error) {
-            res.status(500).json(error);
-        }
+  },
+  // TODO: get one guest
+  async getGuestbyId({ user, params }, res) {
+    try {
+    } catch (error) {
+      res.status(500).json(error);
     }
-    // TODO: get guest count
-    // async createGuest({user, body}, res){
-    //     try {
-            
-    //     } catch (error) {
-    //         res.status(500).json(error);
-    //     }
-    // }
-}
+  },
+  // TODO: get guest count
+  // async createGuest({user, body}, res){
+  //     try {
+
+  //     } catch (error) {
+  //         res.status(500).json(error);
+  //     }
+  // }
+};
