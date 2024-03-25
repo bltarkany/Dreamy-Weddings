2910768ff757000bb043ae4d84b2b2aa6a8aa205
Bobbi Tarkany <bl.tarkany@gmail.com>
2024-03-25 09:30:36 -0700

    create gallery - gallery controllers

--- server/controllers/galleryController.js
+++ server/controllers/galleryController.js
@@ -1,6 +1,47 @@
-// TODO: create gallery item
-// TODO: delete gallery item
-// TODO: update gallery item
-// TODO: get all gallery items
-// TODO: get galley items by event
-// TODO: get single gallery item
+const { Gallery } = require('../models');
+module.exports = {
+  // TODO: create gallery item
+  async createGallery({ user, body }, res) {
+    try {
+      const gallery = await Gallery.create(body);
+      res.status(201).json(gallery);
+    } catch (error) {
+      res.status(500).json();
+    }
+  },
+  // TODO: delete gallery item
+  async deleteGallery({ user }, res) {
+    try {
+    } catch (error) {
+      res.status(500).json();
+    }
+  },
+  // TODO: update gallery item
+  async updateGallery({ user }, res) {
+    try {
+    } catch (error) {
+      res.status(500).json();
+    }
+  },
+  // TODO: get all gallery items
+  async getGalleries({ user }, res) {
+    try {
+    } catch (error) {
+      res.status(500).json();
+    }
+  },
+  // TODO: get galley items by event
+  async getGalleryByEvent({ user }, res) {
+    try {
+    } catch (error) {
+      res.status(500).json();
+    }
+  },
+  // TODO: get single gallery item
+  async getGalleryById({ user }, res) {
+    try {
+    } catch (error) {
+      res.status(500).json();
+    }
+  },
+};
