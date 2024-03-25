1cf1660a4d75f2252ff8532227bbdf4a9adef411
Bobbi Tarkany <bl.tarkany@gmail.com>
2024-03-25 09:15:34 -0700

    created get all - layout controllers

--- server/controllers/layoutControllers.js
+++ server/controllers/layoutControllers.js
@@ -1,15 +1,41 @@
-const { Event, Guest, EventGuest } = require('../models');
+const { Layout } = require('../models');
 
 module.exports = {
   // TODO - get layouts
+  async getLayouts({ user }, res) {
+    try {
+      const layouts = await Layout.findAll();
+      res.status(200).json(layouts);
+    } catch (error) {
+      res.status(500).json();
+    }
+  },
+  // TODO - get by page
   async namehere({ user }, res) {
     try {
     } catch (error) {
       res.status(500).json();
     }
   },
-  // TODO - get by page
   // TODO - create layout
+  async namehere({ user }, res) {
+    try {
+    } catch (error) {
+      res.status(500).json();
+    }
+  },
   // TODO - update layout
+  async namehere({ user }, res) {
+    try {
+    } catch (error) {
+      res.status(500).json();
+    }
+  },
   // TODO - delete layout
+  async namehere({ user }, res) {
+    try {
+    } catch (error) {
+      res.status(500).json();
+    }
+  },
 };
