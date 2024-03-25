17e5ad7a9408241e9cb14e0b0bcd1701ced631c7
Bobbi Tarkany <bl.tarkany@gmail.com>
2024-03-25 08:53:30 -0700

    added create memeber controller - wedding party conctrollers

--- server/controllers/wedpartyControllers.js
+++ server/controllers/wedpartyControllers.js
@@ -1,5 +1,17 @@
-// TODO: create wedding party member
-// TODO: update party member
-// TODO: delete party memeber
-// TODO: get all party
-// TODO: get single party member
+const { WedParty } = require('../models');
+
+module.exports = {
+  // TODO: create wedding party member
+  async createMember({ user, body }, res) {
+    try {
+      const member = await WedParty.create(body);
+      res.status(201).json(member);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  },
+  // TODO: update party member
+  // TODO: delete party member
+  // TODO: get all party
+  // TODO: get single party member
+};
