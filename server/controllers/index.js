b4e4461ee5a5aed9c07f0620e3951cfb28af41ef
Bobbi Tarkany <bl.tarkany@gmail.com>
2024-03-25 09:03:29 -0700

    updated export of wedding party conctrollers

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,5 +1,11 @@
 const userControllers = require('./userControllers');
 const eventControllers = require('./eventControllers');
 const evt_gstControllers = require('./evt_guestControllers');
+const wedPartyControllers = require('./wedpartyControllers');
 
-module.exports = { userControllers, eventControllers, evt_gstControllers };
+module.exports = {
+  userControllers,
+  eventControllers,
+  evt_gstControllers,
+  wedPartyControllers,
+};
