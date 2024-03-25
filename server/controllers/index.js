d53ed31ae561f7991011d2c8070ce3f5d9d2770d
Bobbi Tarkany <bl.tarkany@gmail.com>
2024-03-25 09:26:20 -0700

    updated export - layout controllers

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -2,10 +2,12 @@ const userControllers = require('./userControllers');
 const eventControllers = require('./eventControllers');
 const evt_gstControllers = require('./evt_guestControllers');
 const wedPartyControllers = require('./wedpartyControllers');
+const layoutControllers = require('./layoutControllers');
 
 module.exports = {
   userControllers,
   eventControllers,
   evt_gstControllers,
   wedPartyControllers,
+  layoutControllers,
 };
