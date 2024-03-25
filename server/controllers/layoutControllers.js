4729a8d51257b92107c5fe87236f6860e447ed95
Bobbi Tarkany <bl.tarkany@gmail.com>
2024-03-25 09:19:13 -0700

    created get by page - layout controllers

--- server/controllers/layoutControllers.js
+++ server/controllers/layoutControllers.js
@@ -11,8 +11,17 @@ module.exports = {
     }
   },
   // TODO - get by page
-  async namehere({ user }, res) {
+  async getByPage({ user, params }, res) {
     try {
+      const layouts = await Layout.findAll({
+        where: {
+          page: params.page,
+        },
+      });
+      if (!layouts) {
+        return res.status(404).json({ message: 'No layouts for this page' });
+      }
+      res.status(200).json(layouts);
     } catch (error) {
       res.status(500).json();
     }
