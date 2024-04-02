317e5da33682709a2e8fbdf8f9b5b987d97974aa
Bobbi Tarkany <bl.tarkany@gmail.com>
2024-04-02 11:04:49 -0700

    updated home page layout

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -13,6 +13,7 @@ const defaultImgs = [
   'https://res.cloudinary.com/dvsxq5pie/image/upload/v1711490428/dreamy_wedding/home/home_ring_wvwfle.jpg',
   'https://res.cloudinary.com/dvsxq5pie/image/upload/v1711490278/dreamy_wedding/home/home_coupleback_szz0qa.jpg',
 ];
+const heroImg = `https://res.cloudinary.com/dvsxq5pie/image/upload/v1711490283/dreamy_wedding/home/home_florals_o2zcjv.jpg`;
 
 export default function Home() {
   const [images, setImages] = useState([]);
@@ -23,11 +24,11 @@ export default function Home() {
 
   return (
     <>
-      {/* <SlideCarousel images={images} />
-      <Hero
-        header={`Mr. and Ms. cordially invite you to their upcoming nuptials`}
-      /> */}
-      <HeroWithImg />
+      <HeroWithImg
+        image={heroImg}
+        greeting={`Mr. and Ms. cordially invite you to their upcoming nuptials`}
+        intro={`The happy couple is hope you can attend this beautiful event and Share in there exchanging of vows`}
+      />
     </>
   );
 }
